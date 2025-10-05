d06566b0ebbec283eeaec02d23eacd421fe3b938
Lucy Winters <lucy@wintershome.co.uk>
2025-10-05 17:51:08 +0100

    added template and snippet functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # About
 There are a lot of static site generators. I know this because, frustrated with [Cobalt](https://cobalt-org.github.io/) and [Jekyll](https://jekyllrb.com/), I set about looking for alternatives. I found [many](https://github.com/myles/awesome-static-generators). Rather than weed through them looking for one that behaved the way I wanted it to, I decided to write my own. I assume this same reasoning is why there are so many.
+
+# Guide
+Create a folder for your site source files. Inside it, create directories called `_templates` and `_snippets`. In `_templates`, put HTML templates that you want your Markdown pages to use. Within the templates, you can refer to snippets by writing `%snippet_name%`. Also put `{content}` somewhere; this is where the Markdown-converted-to-HTML will go. In `_snippets`, put HTML files with bits of HTML you want to be able to insert into templates (header, footer, etc.).
+
+Then, write Markdown files. The Markdown files will get converted into HTML and substituted for `{content}` in the relevant template.
--- main.py
+++ main.py
@@ -1,17 +1,92 @@
 import markdown as md
 import argparse
 import os
+from pathlib import Path
 
-def setup_args():
+"""
+Parse command line arguments.
+"""
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help="Show verbose output.", action="store_true")
     parser.add_argument("input", metavar="<INPUT>", help="Specify a site folder to build. Markdown files in this directory will be converted to HTML. Sub-directories starting with '-' will be skipped; all other files will be copied in-place.")
     parser.add_argument("output", metavar="<OUTPUT>", help="Specify a folder for the built site.")
     return parser.parse_args()
 
+
+"""
+Read the template directory and return a list of template strings.
+"""
+def get_templates(site_dir):
+
+    # Get the directory to work with.
+    templates_dir = os.path.join(site_dir, "_templates")
+
+    # Create a list of templates (strings), prior to being formatted.
+    templates = []
+    for template_file in os.listdir(templates_dir):
+        if template_file.endswith(".html"):
+            template_path = os.path.join(templates_dir, template_file)
+            with open(template_path, "r") as f:
+                templates.append(f.read())
+
+    return templates
+
+
+"""
+Create a dictionary of names of snippets and their content.
+"""
+def get_snippets(site_dir):
+
+    # Get the directory to work with.
+    snippets_dir = os.path.join(site_dir, "_snippets")
+
+    snippets = {}
+    for snippet_file in os.listdir(snippets_dir): # Loop through all the snippet files
+        if snippet_file.endswith(".html"):
+            snippet_path = os.path.join(snippets_dir, snippet_file)
+            with open(snippet_path, "r") as f:
+                snippets["%" + Path(snippet_path).stem + "%"] = f.read() # Set the dictionary keys to %[filename]%
+
+    return snippets
+
+
+"""
+Takes a template (string) and dictionary of snippets and substitutes occurences of dict keys with the dict content.
+"""
+def format_template(template, snippets):
+
+    for snippet_name in snippets:
+        template = template.replace(snippet_name, snippets[snippet_name])
+    formatted_template = template
+
+    return formatted_template
+
+
+def convert_md_to_html(input_file):
+    with open(input_file, "r") as f:
+        md_content = f.read()
+
+    md.reset()
+    content = md.convert(md_content)
+
+    return content
+
 def main():
-    args = setup_args()
-    print(os.listdir(args.input))
+    args = parse_args()
+    site_dir = args.input
+    dest_dir = args.output
+
+    templates = get_templates(site_dir)
+    snippets = get_snippets(site_dir)
+
+    formatted_templates = []
+    for template in templates:
+        formatted_templates.append(format_template(template, snippets))
+
+    print(formatted_templates)
+
+
 
 if __name__ == "__main__":
     main()
